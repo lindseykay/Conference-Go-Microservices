0b53b6e083c2ea94d8b574f6970b98acdc69e650
Lindsey <lindseykaycarlson@outlook.com>
2022-10-03 20:41:11 -0700

    attendees_microservice pulls conference info from monolith

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -2,10 +2,16 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
 
-from events.api_views import ConferenceListEncoder
+# from events.api_views import ConferenceListEncoder
 from common.json import ModelEncoder
-from .models import Attendee
-from events.models import Conference
+from .models import Attendee, ConferenceVO
+
+# from events.models import Conference
+
+
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
 
 
 class AttendeeListEncoder(ModelEncoder):
@@ -23,12 +29,12 @@ class AttendeeDetailEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {
-        "conference": ConferenceListEncoder(),
+        "conference": ConferenceVODetailEncoder(),
     }
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -49,7 +55,7 @@ def api_list_attendees(request, conference_id):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
@@ -59,9 +65,10 @@ def api_list_attendees(request, conference_id):
 
         # Get the Conference object and put it in the content dict
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f"/api/conferences/{conference_vo_id}/"  # no idea what's going on here
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
